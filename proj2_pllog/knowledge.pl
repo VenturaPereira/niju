a90df9bbc1ac5dba1f96711881b722cbc78130c4
joaopedrofump <joaofurriel@wifi-interno.fe.up.pt>
2017-12-19 15:01:52 +0000

    :rocket

--- proj2_pllog/knowledge.pl
+++ proj2_pllog/knowledge.pl
@@ -22,27 +22,79 @@ skills([
 %participant(id, name, list of skils, age, height, sex)
 
 participants([
-        [1,'João', [1], 20, 171, 2],
-        [2,'Pedro', [2,5], 12, 179, 2],
-        [3,'Ventura', [3,6], 21, 172,2],
-        [4,'Luis', [4], 14, 167,2],
-        [5,'Vasco', [5], 20, 161,2],
-        [6,'Maria', [7,8], 13, 151,1],
-        [7,'Joana', [6], 15, 167,1],
-        [8,'Manuela', [], 9, 160,1],
-        [9,'Elisabete', [1], 11, 165,1]
+        [1,'João'],
+        [2,'Pedro'],
+        [3,'Ventura'],
+        [4,'Luis'],
+        [5,'Vasco'],
+        [6,'Maria'],
+        [7,'Joana'],
+        [8,'Manuela'],
+        [9,'Elisabete']
     ]).
 
-%activity(id, name, requeried skill, sex, min height, max heihgt, min age, max age (at least one element), number of elements
+participantSkills([
+
+    [1],
+    [5],
+    [3,6],
+    [4],
+    [5],
+    [7,8],
+    [6],
+    [2],
+    [1]
+]).
+
+
+participantPhysicalAtt([
+
+    [20,171,2],
+    [12,179,2],
+    [21,172,2],
+    [14,167,2],
+    [20,161,2],
+    [13,151,1],
+    [15,167,1],
+    [9,160,1],
+    [11,165,1]
+
+
+]).
+
+
+%activity(id, name, required skill, sex, min height, max heihgt, min age, max age (at least one element), number of elements
 activities([
-    [1,'football',[2,5],2,0,300,15,25,6],
-    [2,'cards',[6],1,0,300,16,27,4],
-    [3,'walk',[],1,0,300,10,25,2],
-    [4,'fireworks',[],0,150,300,20,25,2],
-    [5,'build app',[3],0,0,300,18,25,2],
-    [6,'meal',[],0,0,300,0,50,5],
-    [7,'reading',[7,8],0,0,300,0,50,3]
+    [1,'football'],
+    [2,'cards'],
+    [3,'walk'],
+    [4,'fireworks'],
+    [5,'build app'],
+    [6,'meal'],
+    [7,'reading']
     ]).
+
+activitiesSkils([
+
+    [2,5],
+    [6],
+    [],
+    [],
+    [3],
+    [],
+    [7,8]
+]).
+
+activityRequirements([
+
+    [2,0,300,15,25,6],
+    [1,0,300,16,27,4],
+    [1,0,300,10,25,2],
+    [0,150,300,20,25,2],
+    [0,0,300,18,25,2],
+    [0,0,300,0,50,5],
+    [0,0,300,0,50,3]
+]).
 
 
 group('football', ['João', 'Pedro', 'Ventura', 'Luis', 'Manuela', 'Maria']).
--- proj2_pllog/solver.pl
+++ proj2_pllog/solver.pl
@@ -3,31 +3,76 @@
 
 :- include('knowledge.pl').
 
-createGroup(ActivityId, Group) :-
+% createGroup(ActivityId, Group) :-
 
-    activities(Activities),
-    nth1(ActivityId, Activities, Activity),
-    Activity = [_,_,_,_,_,_,_,_,GroupLength],
+%     %activity name
+%     activities(Activities),
+%     nth1(ActivityId, Activities, Activity),
+%     Activity = [_,ActivityName],
+
+
+%     %total of participantes
+%     participants(Participants),
+%     length(Participants, NumberOfParticipants),
+
+%     %activity requirement
+%     activityRequirements(ActivitiesRequirements),
+%     nth1(ActivityId,ActivitiesRequirements,ActivityRequirements),
+
+%     %activity number of elements
+%     nth1(6, ActivityRequirements, GroupLength),
+
+
+%     length(Group, GroupLength),
+%     domain(Group, 1, NumberOfParticipants),
+%     all_distinct(Group),
+
+%     %atividades necessárias
+
+%     % [_,_,MandatorySkills,_,_,_,_,_,_],
+
+%     labeling([],Group).
+
+
+
+% 1-[1], 2-[2,5] 3-[3,6] ...
+
+
+getParticipant(ActivityIndex, Participant) :-
+
+    activityRequirements(ActivitiesRequirements),
+    nth1(ActivityIndex, ActivitiesRequirements, [Sex, MinHeight, MaxHeight, MinAge, MaxAge, _]),
+
+    %activitiesSkils(ActivitiesSkills),
+    %nth1(ActivityIndex, ActivitiesSkills, ActivitySkills),
 
     participants(Participants),
-    length(Participants, NumberOfParticipants),
+    length(Participants,NumberOfParticipants),
+    domain([Participant], 1, NumberOfParticipants),
+
+    checkParticipantPhysicalAtts(Sex,MinHeight,MaxHeight,MinAge,MaxAge,Participant),
+
+    labeling([], [Participant]).
+
+
+
+checkParticipantPhysicalAtts(ReqSex, MinHeight, MaxHeight, MinAge, MaxAge, ParticipantIndex) :-
+
+    participantPhysicalAtt(ParticipantsPhysicalAtts),
+    nth1(ParticipantIndex, ParticipantsPhysicalAtts, ParticipantsPhysicalAtt),
+
+    ParticipantsPhysicalAtt = [Age, Height, Sex],
+    Age #=< MaxAge,
+    Age #>= MinAge,
+    Height #=< MaxHeight,
+    Height #>= MinHeight,
+    Sex #= ReqSex #\/ ReqSex #= 0.
+
+
+
+checkParticipantSkills(Skill, ParticipantIndex) :-
 
     
-    length(Group, GroupLength),
-    domain(Group, 1, NumberOfParticipants),
-    all_distinct(Group),
-
-    %atividades necessárias
-
-    [_,_,MandatorySkills,_,_,_,_,_,_],
-
-    labeling([],Group).
-
-
-
-
-
-
 
 
 %reconsult('/Users/joaofurriel/Documents/Estudo/MIEIC/Ano3/PLOG/Projecto/proj2_pllog/solver.pl').
